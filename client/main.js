1b3f76392025047ed1fc86ef058ff5cf269ca9a5
jangjihoon96 <jangna96@gmail.com>
2023-01-13 15:50:35 +0900

    주접 생성기 완성

--- client/main.js
+++ client/main.js
@@ -1,9 +1,54 @@
-/* eslint no-undef: 'warn' */
-/* eslint no-unused-vars: 'off' */
+
+import { jujeobData } from './data/data.js';
+import { clearContents, getInputValue, getNode, getRandom, insertLast, isNumericString, showAlert, syntaxError, typeError } from './lib/index.js';
 
 
 
-console.log(window)
+const submit = getNode('#submit');
+// const inputValue = getNode('#nameField');
+const resultArea = getNode('.result');
+
+
+
+
+
+
+function clickSubmitHandler(e){
+
+  e.preventDefault();
+
+  let name = getInputValue('#nameField');
+  let list = jujeobData(name);
+  let pick = list[getRandom(list.length - 1)];
+  
+
+  if(!name){
+    showAlert('.alert','이름을 입력해주세요.',3000);
+    return
+  }
+
+  if(isNumericString(name)){
+    showAlert('.alert','제대로된 이름을 입력해주세요.',3000);
+    return
+  }
+
+  clearContents(resultArea)
+
+  insertLast(resultArea,pick);
+
+  console.log(pick)
+
+
+}
+
+
+function mouseClickPasty(e){
+  return resultArea.textContent.execCommand('copy');
+}
+
+
+submit.addEventListener('click', clickSubmitHandler);
+resultArea.addEventListener('click', mouseClickPasty);
 
 
 
