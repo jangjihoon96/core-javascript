ed00458bc2c9fbb037f36a4dca341dd619793ef0
jangjihoon96 <jangna96@gmail.com>
2023-01-16 14:00:18 +0900

    정확한 validation 유틸추가 (typeOf.js)

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
-import { diceAnimation, getNode } from "./lib/index.js";
+import { diceAnimation, disableElement, enableElement, getNode, getNodes, invisibleElement, visibleElement } from "./lib/index.js";
 
 
 // [ 주사위 굴리기 ] {#fff}
@@ -7,29 +7,63 @@ import { diceAnimation, getNode } from "./lib/index.js";
 // 3. handlerRollingDice 함수 만들고 토글로 애니메이션 제어하기
 // 4. 변수 보호를 위한 클로저 + IIFE 사용하기
 
+// [ 레코드 리스트 보이기 ] {#fff}
+// 1. handleRecord 함수를 만들기
+// 2. disable 활성 유틸 함수 만들기
+// 3. handleReset 함수를 만듭니다.
+// 4. visible 활성 유틸 함수 만들기
+// 5. toggleState 유틸 함수 만들기 
 
-const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)');
+
+// 배열의 구조 분해 할당
+const [rollingDiceButton,recordButton,resetButton] = getNodes('.buttonGroup > button');
+const recordListWrapper = getNode('.recordListWrapper');
+
+
+// const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)');
+// const recordButton = getNode('.buttonGroup > button:nth-child(2)');
+// const resetButton = getNode('.buttonGroup > button:nth-child(3)');
+
 
 
 // IIFE
 
-const handlerRollingDice = (() => {
+const handleRollingDice = (() => {
   let stopAnimation;
   let isRolling = false;
 
   return () => {
     if(!isRolling){
       stopAnimation = setInterval(diceAnimation, 100);
+      // recordButton.disabled = true;
+      disableElement(recordButton)
+      disableElement(resetButton)
       // isRolling = true;
     } else {
       clearInterval(stopAnimation);
+      // recordButton.disabled = false;
+      enableElement(recordButton);
+      enableElement(resetButton);
       // isRolling = false;
     }
     isRolling = !isRolling;
   }
-  
+
 })()
 
+const handleRecord = () => {
+  
+  visibleElement(recordListWrapper);
 
-rollingDiceButton.addEventListener('click',handlerRollingDice)
+}
+
+const handleReset = () => {
+
+  invisibleElement(recordListWrapper);
+  
+}
+
+rollingDiceButton.addEventListener('click',handleRollingDice)
+recordButton.addEventListener('click',handleRecord)
+resetButton.addEventListener('click',handleReset)
 
