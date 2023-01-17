c901539923704e6e20c1f79ca48978e6a79df5e8
jangjihoon96 <jangna96@gmail.com>
2023-01-17 15:06:52 +0900

    [추가] get,post,put,delete

--- client/lib/utils/xhr.js
+++ client/lib/utils/xhr.js
@@ -10,29 +10,46 @@
 
 
 
-function xhrData(method,url,body){
+function xhrData({
+  url = '',
+  method = 'GET',
+  body = null,
+  onSuccess = null,
+  onFail = null,
+  headers = {
+    'Content-Type':'application/json',
+    'Access-Control-Allow-Origin':'*',
+  }
+} = {}){
+  // const{method,url,body} = options
 
   const xhr = new XMLHttpRequest();
 
   xhr.open(method,url);
 
-  
-  
+  // Object.entries(headers).forEach(([key,value])=>{
+  //   // console.log(key,value)
+  //   xhr.setRequestHeader(key,value);
+  // })
+
+
 
   xhr.addEventListener('readystatechange', () => {
 
     // 객체 구조분해 할당
     const {status,readyState,response} = xhr;
-    
+
     // console.log(xhr.readyState)
     // console.log(xhr.status)
     if(status >= 200 && status < 400){
       if(readyState === 4){
         console.log('통신 성공');
-        console.log(JSON.parse(response))
+        onSuccess(JSON.parse(response))
+        // console.log(JSON.parse(response))
       }
     } else {
-      console.error('통신 실패');
+      onFail('통신 실패')
+      // console.error('통신 실패');
     }
   });
 
@@ -44,30 +61,115 @@ function xhrData(method,url,body){
 
 
 // 비동기 통신 오픈
-xhrData('POST','https://jsonplaceholder.typicode.com/users',{
-  // post 통신은 새롭게 생성하는 것, 즉 id값을 넣지않음 (들어가면 생성됨)
-  // "id": 1,
-  "name": "Jihoon",
-  "username": "jangjihoon",
-  "email": "Sincere@april.biz",
-  "address": {
-    "street": "Kulas Light",
-    "suite": "Apt. 556",
-    "city": "Gwenborough",
-    "zipcode": "92998-3874",
-    "geo": {
-      "lat": "-37.3159",
-      "lng": "81.1496"
-    }
+
+
+xhrData.get = (url,onSuccess,onFail) => {
+  xhrData({
+    url,
+    onSuccess,
+    onFail
+  })
+}
+
+xhrData.post = (url,body,onSuccess,onFail) => {
+  xhrData({
+    method:'POST',
+    body,
+    url,
+    onSuccess,
+    onFail
+  })
+}
+
+xhrData.put = (url,body,onSuccess,onFail) => {
+  xhrData({
+    method:'PUT',
+    body,
+    url,
+    onSuccess,
+    onFail
+  })
+}
+
+xhrData.delete = (url,onSuccess,onFail) => {
+  xhrData({
+    method:'DELETE',
+    url,
+    onSuccess,
+    onFail
+  })
+}
+
+
+xhrData.get(
+  'https://jsonplaceholder.typicode.com/users/1',
+  (result)=>{
+    console.log(result);
   },
-  "phone": "010-0000-0000",
-  "website": "hildegard.org",
-  "company": {
-    "name": "Romaguera-Crona",
-    "catchPhrase": "Multi-layered client-server neural-net",
-    "bs": "harness real-time e-markets"
+  (err)=>{
+    console.log(err);
   }
-});
+)
+
+xhrData.post(
+  'https://jsonplaceholder.typicode.com/users',
+  {
+    "name" : "Ji Hoon",
+    "username" : "hooon"
+  },
+  (result)=>{
+    console.log(result);
+  },
+  (err)=>{
+    console.log(err);
+  }
+)
+
+
+
+// xhrData({
+//   url: 'https://jsonplaceholder.typicode.com/users/1',
+//   onSuccess: (result) => {
+//     console.log(result);
+//   },
+//   onFail: (err) => {
+//     console.error(err);
+//   }
+// });
+
+// xhrData({
+//   url:'https://jsonplaceholder.typicode.com/users',
+//   // method:'GET',
+//   // body:null,
+//   headers:{
+//     'Content-Type':'application/json'
+//   }
+// })
+
+// xhrData('POST','https://jsonplaceholder.typicode.com/users',{
+//   // post 통신은 새롭게 생성하는 것, 즉 id값을 넣지않음 (들어가면 생성됨)
+//   // "id": 1,
+//   "name": "Jihoon",
+//   "username": "jangjihoon",
+//   "email": "Sincere@april.biz",
+//   "address": {
+//     "street": "Kulas Light",
+//     "suite": "Apt. 556",
+//     "city": "Gwenborough",
+//     "zipcode": "92998-3874",
+//     "geo": {
+//       "lat": "-37.3159",
+//       "lng": "81.1496"
+//     }
+//   },
+//   "phone": "010-0000-0000",
+//   "website": "hildegard.org",
+//   "company": {
+//     "name": "Romaguera-Crona",
+//     "catchPhrase": "Multi-layered client-server neural-net",
+//     "bs": "harness real-time e-markets"
+//   }
+// });
 
 
 
